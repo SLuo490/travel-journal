9f54eb40e0155de1359685f1e73b4d77f2b7b192
SLuo5991 <sluo5991@gmail.com>
2022-08-30 16:28:33 -0400

    add more data

--- src/data.js
+++ src/data.js
@@ -9,4 +9,24 @@ export default [
       'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.',
     imageUrl: 'https://source.unsplash.com/WLxQvbMyfas',
   },
+  {
+    title: 'New York City',
+    location: 'United States',
+    googleMapsUrl: 'https://goo.gl/maps/KBHAP3vEpuKqboJJ9',
+    startDate: '12 Jan, 2021',
+    endDate: '24 Jan, 2021',
+    description:
+      'New York City is the most populous city in the United States and the center of the New York metropolitan area, the most populous metropolitan area in the United States.',
+    imageUrl: 'https://source.unsplash.com/kheTI8pIywU',
+  },
+  {
+    title: 'Paris',
+    location: 'France',
+    googleMapsUrl: 'https://goo.gl/maps/h5XKR8SqatqvpZtQ8',
+    startDate: '12 Jan, 2021',
+    endDate: '24 Jan, 2021',
+    description:
+      'Paris is the capital of France, and the largest city of France. The Paris area is one of the largest population centers in Europe, with more than 12 million inhabitants.',
+    imageUrl: 'https://source.unsplash.com/nnzkZNYWHaU',
+  },
 ];
