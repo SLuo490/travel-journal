edacaea5062c6158ad1ce5e5646ad8d7262cb629
SLuo5991 <sluo5991@gmail.com>
2022-08-30 16:44:13 -0400

    add card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar';
+import Card from './components/Card';
 import './style.css';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+import '../style.css';
+
+export default function Card() {
+  return (
+    <div>
+      <h1>Card</h1>
+    </div>
+  );
+}
