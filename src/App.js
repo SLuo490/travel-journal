8e2911096f8a8ed10bdb28ad7a3b8350e317b962
Shi Tao Luo <sluo5991@gmail.com>
2022-08-31 22:32:52 -0400

    add props to card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Navbar from './components/Navbar';
 import Card from './components/Card';
+import data from './data';
 import './style.css';
 
 function App() {
+  const cardElements = data.map((item) => {
+    return <Card key={item.id} item={item} />;
+  });
+
   return (
     <div className='App'>
       <Navbar />
-      <Card />
+      {cardElements}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,31 +2,27 @@ import '../style.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className='card-container'>
       <img
         className='location-img'
-        src='https://source.unsplash.com/WLxQvbMyfas'
-        alt='image of place'
+        src={props.item.imageUrl}
+        alt='pic of place'
       />
       <div className='info'>
         <div className='location'>
           <FontAwesomeIcon className='fa-location-icon' icon={faLocationDot} />
-          <span className='location-country'>JAPAN</span>
-          <a href='https://goo.gl/maps/1DGM5WrWnATgkSNB8'>View on Google Map</a>
+          <span className='location-country'>{props.item.location}</span>
+          <a href={props.item.googleMapsUrl}>View on Google Map</a>
         </div>
         <div className='location-info'>
-          <h1 className='location-title'>Mount Fuji</h1>
+          <h1 className='location-title'>{props.item.title}</h1>
           <h3 className='date'>
-            <span className='startDate'>12 Jan, 2021</span>-
-            <span className='endDate'>24 Jan, 2021</span>
+            <span className='startDate'>{props.item.startDate}</span>-
+            <span className='endDate'>{props.item.endDate}</span>
           </h3>
-          <p className='description'>
-            Mount Fuji is the tallest mountain in Japan, standing at 3,776
-            meters (12,380 feet). Mount Fuji is the single most popular tourist
-            site in Japan, for both Japanese and foreign tourists.
-          </p>
+          <p className='description'>{props.item.description}</p>
         </div>
       </div>
     </div>
